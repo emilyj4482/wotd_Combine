f5dc71cefff96685c906ea74e0a0fe61db2a3666
emilyj4482 <emilyj4482@gmail.com>
2024-05-21 01:31:22 +0900

    first commit

---
+++ wotd_Combine/wotd_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  wotd_CombineApp.swift
+//  wotd_Combine
+//
+//  Created by EMILY on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct wotd_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
